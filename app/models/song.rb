bb62f46e6f105975d3187227026553d574706762
Travis Exline <t.exline22@gmail.com>
2019-07-25 14:26:19 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,5 @@ class Song < ApplicationRecord
   validates :title, presence: true
   validates :title, uniqueness: true
   validates :released, inclusion: { in: [true, false]}
+  validates :artist_name, presence: true
 end
